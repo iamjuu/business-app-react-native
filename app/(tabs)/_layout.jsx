546f9ea3e083061e4007f1df7fd528925d363762
Muhammed Ajmal CC <muhammedajmalcc6424094@gmail.com>
2025-01-07 11:07:54 +0530

    layout created

---
+++ app/(tabs)/_layout.jsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Tabs } from 'expo-router'
+export default function TabLayout() {
+  return (
+    <Tabs>
+<Tabs.Screen name='home'/>
+<Tabs.Screen name='about'/>
+<Tabs.Screen name='explore'/>
+<Tabs.Screen name='profle'/>
+    </Tabs>
+  )
+}
